be726c06a14ebfdbc55328d35067a65c28829cd8
寞踪 <361131953@qq.com>
2016-01-16 16:27:26 +0800

    add smarty2.6

--- framework/core/View.php
+++ framework/core/View.php
@@ -31,7 +31,6 @@ class View
      */
     public function __construct(){
         $app = Wave::app();
-        // require $app->frameworkPath.'Library/Smarty/Smarty.class.php';
         $dir = $app->projectPath.$app->projectName;
 
         $config = Wave::app()->config['smarty'];
