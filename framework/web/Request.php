9d4fbf47af4873e1720293f00e5d7a0bb0262143
xuping <xuping@quakegame.cn>
2016-05-20 10:17:27 +0800

    request 修改

--- framework/web/Request.php
+++ framework/web/Request.php
@@ -327,22 +327,19 @@ class Request
      */
     public static function getClientIp()
     {
-         if( isset($_SERVER) ){
-            if (isset($_SERVER['HTTP_CDN_SRC_IP'])) {
+        $realip = '';
+        if(isset($_SERVER)){
+            if(isset($_SERVER['HTTP_CDN_SRC_IP'])) {
                 $realip = $_SERVER["HTTP_CDN_SRC_IP"];
-            } else if( isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ) {
-                $realip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-            } elseif ( isset($_SERVER["HTTP_CLIENT_IP"]) ) {
+            }elseif (isset($_SERVER["HTTP_CLIENT_IP"])) {
                 $realip = $_SERVER["HTTP_CLIENT_IP"];
-            } elseif ( isset($_SERVER["REMOTE_ADDR"]) ) {
+            } elseif (isset($_SERVER["REMOTE_ADDR"])) {
                 $realip = $_SERVER["REMOTE_ADDR"];
             } else {
                 $realip = $_SERVER["SSH_CLIENT"];
             }
-        } else {
-            if( getenv("HTTP_X_FORWARDED_FOR") ){
-                $realip = getenv("HTTP_X_FORWARDED_FOR");
-            } elseif ( getenv("HTTP_CLIENT_IP") ) {
+        }else{
+            if (getenv("HTTP_CLIENT_IP")) {
                 $realip = getenv("HTTP_CLIENT_IP");
             } else {
                 $realip = getenv("REMOTE_ADDR");
