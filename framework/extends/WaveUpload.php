78a3e2390d74da3bc56d1070d330fab3330c33cb
xuping <xuping@quakegame.cn>
2016-05-23 16:56:19 +0800

    上传类

---
+++ framework/extends/WaveUpload.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PHP 5.0 以上
+ * 
+ * @package         Wavephp
+ * @author          许萍
+ * @copyright       Copyright (c) 2016
+ * @link            https://github.com/xpmozong/wavephp2
+ * @since           Version 2.0
+ *
+ */
+
+/**
+ * Wavephp Application WaveUpload Class
+ *
+ * 框架上传类
+ *
+ * @package         Wavephp
+ * @subpackage      core
+ * @author          许萍
+ *
+ */
+class WaveUpload
+{
+    /**
+     * 上传函数
+     *
+     * @param string $saveDir       上传目录
+     * @param string $allowExts     允许上传的文件
+     * @param int $maxSize          上传大小
+     * 
+     * @return array 
+     */
+    public function upload($saveDir, $allowExts, $maxSize)
+    {
+
+    }
+}
+?>
