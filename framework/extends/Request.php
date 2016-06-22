f085ad2c949180a24298b8180b1895fa0f4df698
xuping <xuping@quakegame.cn>
2016-06-22 17:56:57 +0800

    获取IP修改

--- framework/extends/Request.php
+++ framework/extends/Request.php
@@ -327,26 +327,24 @@ class Request
      */
     public static function getClientIp()
     {
-        $realip = '';
-        if(isset($_SERVER)){
-            if(isset($_SERVER['HTTP_CDN_SRC_IP'])) {
-                $realip = $_SERVER["HTTP_CDN_SRC_IP"];
-            }elseif (isset($_SERVER["HTTP_CLIENT_IP"])) {
-                $realip = $_SERVER["HTTP_CLIENT_IP"];
-            } elseif (isset($_SERVER["REMOTE_ADDR"])) {
-                $realip = $_SERVER["REMOTE_ADDR"];
-            } else {
-                $realip = $_SERVER["SSH_CLIENT"];
-            }
-        }else{
-            if (getenv("HTTP_CLIENT_IP")) {
-                $realip = getenv("HTTP_CLIENT_IP");
-            } else {
-                $realip = getenv("REMOTE_ADDR");
-            }
+        $ip = '';
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) { 
+            $ip = $_SERVER["HTTP_X_FORWARDED_FOR"]; 
+        } elseif (isset($_SERVER["HTTP_CLIENT_IP"])) { 
+            $ip = $_SERVER["HTTP_CLIENT_IP"]; 
+        } elseif (isset($_SERVER["REMOTE_ADDR"])) { 
+            $ip = $_SERVER["REMOTE_ADDR"]; 
+        } elseif (getenv("HTTP_X_FORWARDED_FOR")) { 
+            $ip = getenv("HTTP_X_FORWARDED_FOR"); 
+        } elseif (getenv("HTTP_CLIENT_IP")) { 
+            $ip = getenv("HTTP_CLIENT_IP"); 
+        } elseif (getenv("REMOTE_ADDR")) { 
+            $ip = getenv("REMOTE_ADDR"); 
+        } else { 
+            $ip = "Unknown";
         }
 
-        return addslashes($realip);
+        return addslashes($ip);
     }
 
     /**
