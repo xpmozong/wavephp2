995665b896fe835299da9aa1e9c4572578494128
xuping <xuping@quakegame.cn>
2017-07-14 14:38:22 +0800

    防sql注入

--- framework/db/Model.php
+++ framework/db/Model.php
@@ -380,19 +380,19 @@ class Model
      */
     public function safe_replace($string)
     {
-        $string = str_replace('%20','',$string);
-        $string = str_replace('%27','',$string);
-        $string = str_replace('%2527','',$string);
-        $string = str_replace('*','',$string);
-        $string = str_replace('"','"',$string);
-        $string = str_replace("'",'',$string);
-        $string = str_replace('"','',$string);
-        $string = str_replace(';','',$string);
-        $string = str_replace('<','<',$string);
-        $string = str_replace('>','>',$string);
-        $string = str_replace("{",'',$string);
-        $string = str_replace('}','',$string);
-        $string = str_replace('\\','',$string);
+        $string = str_replace('*', '', $string);
+        $string = str_replace('"', '"', $string);
+        $string = str_replace("'", '', $string);
+        $string = str_replace('"', '', $string);
+        $string = str_replace(';', '', $string);
+        $string = str_replace('<', '<', $string);
+        $string = str_replace('>', '>', $string);
+        $string = str_replace("{", '', $string);
+        $string = str_replace('}', '', $string);
+        $string = str_replace('\\', '', $string);
+        $string = str_replace('%20', '', $string);
+        $string = str_replace('%27', '', $string);
+        $string = str_replace('%2527', '', $string);
 
         return $string;
     }
